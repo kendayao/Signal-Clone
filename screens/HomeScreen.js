e66541daaaef2eaa5a53d23b331f553914fecf3d
Kaeneth Dayao <kendayao@gmail.com>
2021-01-28 12:52:35 -0800

    added avatar and title to homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,31 @@
-import React from 'react'
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, {useLayoutEffect} from 'react'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { Avatar } from 'react-native-elements'
+import CustomListItem from '../components/CustomListItem'
+import {auth, db }from '../firebase'
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: 'Signal',
+            headerStyle: { backgroundColor: "#fff"},
+            headerTitleStyle: {color: 'black'},
+            headerTintColor: 'black',
+            headerLeft: ()=>(
+                <View style={{marginLeft: 20}}>
+                    <TouchableOpacity>
+                        <Avatar rounded source={{uri: auth?.currentUser?.photoURL}} />
+                    </TouchableOpacity>
+                </View>
+            )
+        });
+    }, [])
+
     return (
         <SafeAreaView>
             <ScrollView>
-
+                <CustomListItem/>
             </ScrollView>
         </SafeAreaView>
     )
