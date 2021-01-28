ff506e360cb4912420bcc833f48fd124f7ffe513
Kaeneth Dayao <kendayao@gmail.com>
2021-01-27 18:34:27 -0800

    react native starting files

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDDeXQqB03gBlpmIeWSqyDiBMcKXY39kJs",
+    authDomain: "signal-clone-53de0.firebaseapp.com",
+    projectId: "signal-clone-53de0",
+    storageBucket: "signal-clone-53de0.appspot.com",
+    messagingSenderId: "584890459835",
+    appId: "1:584890459835:web:806897f70dcf34d12f6b9e"
+  };
